9eac586936aa3131144d7f253b43dc8c4c7a820c
Saif Ali <this.saif.email@gmail.com>
2024-12-13 22:26:59 +0530

    echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpListener};
+
+
+
+
+#[tokio::main]
+async fn main() {
+
+    //tcp listener
+    let listener = TcpListener::bind("localhost:3001").await.unwrap();
+    let(mut socket, _) = listener.accept().await.unwrap();
+
+    loop {
+
+        let mut buffer = [0u8; 1024];
+    
+        let bytes_read = socket.read(&mut buffer).await.unwrap();
+        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+    }
 }
